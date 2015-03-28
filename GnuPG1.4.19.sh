0742cdc88cdada0ed11596c591de8065b29071c3
mathwhiz1212 <mathwhiz1212@users.noreply.github.com>
2015-03-28 15:33:19 -0600

    Update GnuPG1.4.19.sh

--- GnuPG1.4.19.sh
+++ GnuPG1.4.19.sh
@@ -1,4 +1,4 @@
-#!/bin/bash.
+#!/bin/bash
 #License GPLv3 GNU GPL version 3 <http://gnu.org/licenses/gpl.html>
 #This is free software: you are free to change and redistribute it.
 #There is NO WARRANTY, to the extent permitted by law.
