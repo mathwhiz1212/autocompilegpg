26f00f9269c4d481eb10d127093162d7d80fd83f
mathwhiz1212 <mathwhiz1212@gmail.com>
2015-03-30 15:36:08 -0600

    Offline version

--- GnuPG1.4.19offline.sh
+++ GnuPG1.4.19offline.sh
@@ -6,28 +6,14 @@
 #This script installs GnuPG-1.4.19 or upgrades current version to GnuPG-1.4.19.
 
 #Installs necessary software.
-unzip debs.zip
 cd debs
 dpkg -i *.deb
 cd ..
-
-#Downloads install files.
-wget ftp://ftp.gnupg.org/gcrypt/gnupg/gnupg-1.4.19.tar.bz2
 
 #Asks user if they have installed gpg.
 read -r -p "Do you have a trusted version of GnuPG installed? [y/n] " response
 case $response in
     [yY][eE][sS]|[yY]) 
-        #Attempts to download and import keys needed to verify the integrity of the files
-        gpg --keyserver keys.gnupg.net --recv-keys 4F25E3B6 
-        gpg --keyserver keys.gnupg.net --recv-keys E0856959
-        gpg --keyserver keys.gnupg.net --recv-keys 33BD3F06
-        gpg --keyserver keys.gnupg.net --recv-keys 7EFD60D9
-
-        #Attempts to download signature and to verify the integrity of the files.
-        wget ftp://ftp.gnupg.org/gcrypt/gnupg/gnupg-1.4.19.tar.bz2.sig
-        gpg --verify gnupg-1.4.19.tar.bz2.sig gnupg-1.4.19.tar.bz2
-        sleep 7
 
         #Extracts and compiles GnuPG.
         tar -xvjf gnupg-1.4.19.tar.bz2
@@ -48,12 +34,6 @@ case $response in
         rm gnupg-1.4.19.tar.bz2.sig
         ;;
     *)
-        #Attempts to verify the integrity of the files with sha1sum.
-        echo "Please check this webpage and see if the sha1 checksum for gnupg-1.4.19.tar.bz2 is the same as given here."
-        sha1sum gnupg-1.4.19.tar.bz2
-        sleep 3
-        firefox https://www.gnupg.org/download/integrity_check.html  </dev/null &>/dev/null &
-        read -p "Then hit enter" response
 
         #Extracts and compiles GnuPG.
         tar -xvjf gnupg-1.4.19.tar.bz2
